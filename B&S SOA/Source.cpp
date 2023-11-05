4953f3f6ef138014f999efecd64da6c0427fc3ae
4885226 <ivan.alputov@mail.ru>
2023-11-05 16:06:22 +0300

    работает

--- B&S SOA/Source.cpp
+++ B&S SOA/Source.cpp
@@ -7,29 +7,29 @@ float price = 0.0f;
 
 int main(int argc, char* argv[])
 {
-	//if (argc == 1)
-	//{
-	//	std::cout << "no arguments!" << std::endl;
-	//}
-	//else
-	//{
-	//	for (int i = 0; i < argc; i++)
-	//	{
-	//		std::cout << argv[i];
-	//	}
-	//	N = std::atoi(argv[1]);
-	//}
-	std::cout << "Enter the number of shares:" << std::endl;
-	std::cin >> N;
+	if (argc == 1)
+	{
+		std::cout << "no arguments!" << std::endl;
+	}
+	else
+	{
+		for (int i = 0; i < argc; i++)
+		{
+			std::cout << argv[i]<<" ";
+		}
+		N = std::atoi(argv[1]);
+	}
+	/*std::cout << "Enter the number of shares:" << std::endl;
+	std::cin >> N;*/
 	Option Obj(N);
 	Obj.random_datas();
-	auto start = std::chrono::high_resolution_clock::now();
+	const std::chrono::time_point<std::chrono::system_clock> t1 = std::chrono::system_clock::now();
 	Obj.Get_option_price();
-	auto end = std::chrono::high_resolution_clock::now();
-	std::chrono::duration<float>elapsed = end - start;
+	const std::chrono::time_point<std::chrono::system_clock> t2 = std::chrono::system_clock::now();
+	auto time = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
 	std::cout << " The option price has been calculated!" << std::endl;
 	price = Obj.Get_out_price();
 	std::cout << "Fair option price =" << price << "$"<<std::endl;
-	std::cout << "Elapsed time:" << elapsed.count() << std::endl;
+	std::cout << "Elapsed time:" << time << std::endl;
 
 }
