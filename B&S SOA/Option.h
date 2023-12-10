b10ac98a7f56967e567134b6ff69796fd5eb0b95
4885226 <ivan.alputov@mail.ru>
2023-12-10 21:19:11 +0300

    привет русик

--- B&S SOA/Option.h
+++ B&S SOA/Option.h
@@ -22,13 +22,16 @@ public:
 		K = new float[N];
 		C = new float[N];
 		S0 = new float[N];
-        #pragma omp simd
-		for (int i = 0; i < N; i++)
+#pragma omp parallel num_threads(12)
 		{
-			T[i] = 0.0f;
-			K[i] = 0.0f;
-			C[i] = 0.0f;
-			S0[i] = 0.0f;
+#pragma omp for simd
+			for (int i = 0; i < N; i++)
+			{
+				T[i] = 0.0f;
+				K[i] = 0.0f;
+				C[i] = 0.0f;
+				S0[i] = 0.0f;
+			}
 		}
 
 	}
@@ -46,41 +49,54 @@ public:
 		K = new float[N];
 		C = new float[N];
 		S0 = new float[N];
-        #pragma omp simd
-		for (int i = 0; i < N; i++)
+#pragma omp parallel num_threads(12)
 		{
-			T[i] = 0.0f;
-			K[i] = 0.0f;
-			C[i] = 0.0f;
-			S0[i] = 0.0f;
+#pragma omp for simd
+			for (int i = 0; i < N; i++)
+			{
+				T[i] = 0.0f;
+				K[i] = 0.0f;
+				C[i] = 0.0f;
+				S0[i] = 0.0f;
+			}
 		}
 	}
 	void random_datas()
 	{
 		
-		std::default_random_engine rd(0);//генератор случайных чисел
-		std::uniform_real_distribution<float> dist1(10.0f, 100.0f);
-		std::uniform_real_distribution<float> dist2(5.0f, 30.0f);
-        #pragma omp simd
-		for (int i = 0; i < N; i++)
+		//std::default_random_engine rd(0);//генератор случайных чисел
+		//std::uniform_real_distribution<float> dist1(10.0f, 100.0f);
+		//std::uniform_real_distribution<float> dist2(5.0f, 30.0f);
+#pragma omp parallel num_threads(12)
 		{
+			std::default_random_engine rd(0);//генератор случайных чисел
+			std::uniform_real_distribution<float> dist1(10.0f, 100.0f);
+			std::uniform_real_distribution<float> dist2(5.0f, 30.0f);
+#pragma omp for simd
+			for (int i = 0; i < N; i++)
+			{
 
-			S0[i] = dist1(rd);
-			K[i] = S0[i] + dist2(rd);
-			T[i] = dist2(rd);
-			/*S0[i] = 500.0f * std::rand() / (float)RAND_MAX;
-			K[i] = 500.0f * std::rand() / (float)RAND_MAX;
-			T[i] = 500.0f * std::rand() / (float)RAND_MAX;*/
-			
+				S0[i] = dist1(rd);
+				K[i] = S0[i] + dist2(rd);
+				T[i] = dist2(rd);
+
+
+			}
 		}
 	}
 	void Get_option_price();
 	float Get_out_price()
 	{
 		float price = 0.0f;
-		for (int i = 0; i < N; i++)
+#pragma omp parallel num_threads(12)
 		{
-			price += C[i];
+#pragma omp for simd reduction(+:price)//потоки мешать не будут, каждый поток работает со своей копией price
+		
+			for (int i = 0; i < N; i++)
+			{
+             /*  #pragma omp atomic*/
+				price += C[i];
+			}
 		}
 	
 		return price;
