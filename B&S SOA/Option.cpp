d76daac7070f295bfb0756a621db95dbd58612de
4885226 <ivan.alputov@mail.ru>
2024-03-24 18:20:38 +0300

    рабочая версия

--- B&S SOA/Option.cpp
+++ B&S SOA/Option.cpp
@@ -1,86 +1,73 @@
 #include "Option.h"
-
 void Option::Get_option_price()
 {
 
 
-	int vl = vsetvlmax_e32m4();
-	vfloat32m4_t Int_rest = vfmv_v_f_f32m4(Interest_rest, vl);
-	vfloat32m4_t Volat = vfmv_v_f_f32m4(Volatility, vl);
-	vfloat32m4_t constant1 = vfmv_v_f_f32m4(Interest_rest+(Volatility * Volatility * 0.5f), vl);
-	vfloat32m4_t constant2 = vfmv_v_f_f32m4(Interest_rest - (Volatility * Volatility * 0.5f), vl);
-	vfloat32m4_t Minus_Int_rest = vfmv_v_f_f32m4(Interest_rest * (-1.0f), vl);
-	float* logmas = new float[4];
-	float* sqrtmas = new float[4];
-	float* expmas = new float[4];
-	for (; N > 0; N -= vl)
+	int vl = vsetvlmax_e32m1();
+	vfloat32m1_t Int_rest = vfmv_v_f_f32m1(Interest_rest, vl);
+	vfloat32m1_t Volat = vfmv_v_f_f32m1(Volatility, vl);
+	vfloat32m1_t constant1 = vfmv_v_f_f32m1(Interest_rest + (Volatility * Volatility * 0.5f), vl);
+	vfloat32m1_t constant2 = vfmv_v_f_f32m1(Interest_rest - (Volatility * Volatility * 0.5f), vl);
+	vfloat32m1_t Minus_Int_rest = vfmv_v_f_f32m1(Interest_rest * (-1.0f), vl);
+
+	float logmas[4], expmas[4], sqrtmas[4];
+	float* pC = C, * pK = K, * pS0 = S0, * pT = T;
+	int n = N;
+	//printf("!!!!!!!!!!!!!!!\%d", (int)N);
+	for (; n > 0; n -= vl)
 	{
-		vl = vsetvl_e32m4(N);
-		vfloat32m4_t so = vle_v_f32m4(S0, vl);
-		vfloat32m4_t k = vle_v_f32m4(K, vl);
-		vfloat32m4_t so_dibv_k = vfdiv_vv_f32m4(so, k, vl);
-		vse_v_f32m4(logmas, so_dibv_k, vl);
+		vl = vsetvl_e32m1(n);
+		//printf("!!!!!!!!!!!!!!!\%d", (int)vl);
+		vfloat32m1_t so = vle_v_f32m1(pS0, vl);
+		vfloat32m1_t k = vle_v_f32m1(pK, vl);
+		vfloat32m1_t so_dibv_k = vfdiv_vv_f32m1(so, k, vl);
+		vse_v_f32m1(logmas, so_dibv_k, vl);
 		for (int i = 0; i < 4; i++)
 		{
 			logmas[i] = std::log(logmas[i]);
 		}
-		vfloat32m4_t log_div_sok = vle_v_f32m4(logmas, vl);
-		
-		vfloat32m4_t t = vle_v_f32m4(T, vl);
-		vfloat32m4_t const_mult_T = vfmult_vv_f32m4(t, constant1, vl);
-		    
-		vse_v_f32m4(sqrtmas,t, vl);
+		vfloat32m1_t log_div_sok = vle_v_f32m1(logmas, vl);
+
+		vfloat32m1_t t = vle_v_f32m1(pT, vl);
+		vfloat32m1_t const_mult_T = vfmul_vv_f32m1(t, constant1, vl);
+
+		vse_v_f32m1(sqrtmas, t, vl);
 		for (int i = 0; i < 4; i++)
 		{
 			sqrtmas[i] = std::sqrt(sqrtmas[i]);
 		}
-		vfloat32m4_t sqrt_t = vle_v_f32m4(sqrtmas, vl);
-		vfloat32m4_t vol_mult_sqrt_t = vfmult_vv_f32m4(sqrt_t, Volat, vl);
-		vfloat32m4_t const_mult_T_div_vol_mult_sqrt_t = vfdiv_vv_f32m4(const_mult_T, vol_mult_sqrt_t, vl);
-		vfloat32m4_t d1 = vfadd_vv_f32m4(const_mult_T_div_vol_mult_sqrt_t, log_div_sok);
-		 
-		vfloat32m4_t const_mult_T2 = vfmult_vv_f32m4(t, constant2, vl);
-		vfloat32m4_t const_mult_T_div_vol_mult_sqrt_t2 = vfdiv_vv_f32m4(const_mult_T2, vol_mult_sqrt_t, vl);
-		vfloat32m4_t  d2 = vfadd_vv_f32m4(const_mult_T_div_vol_mult_sqrt_t2, log_div_sok);
+		vfloat32m1_t sqrt_t = vle_v_f32m1(sqrtmas, vl);
+		vfloat32m1_t vol_mult_sqrt_t = vfmul_vv_f32m1(sqrt_t, Volat, vl);
+		vfloat32m1_t const_mult_T_div_vol_mult_sqrt_t = vfdiv_vv_f32m1(const_mult_T, vol_mult_sqrt_t, vl);
+		vfloat32m1_t d1 = vfadd_vv_f32m1(const_mult_T_div_vol_mult_sqrt_t, log_div_sok, vl);
+
+		vfloat32m1_t const_mult_T2 = vfmul_vv_f32m1(t, constant2, vl);
+		vfloat32m1_t const_mult_T_div_vol_mult_sqrt_t2 = vfdiv_vv_f32m1(const_mult_T2, vol_mult_sqrt_t, vl);
+		vfloat32m1_t  d2 = vfadd_vv_f32m1(const_mult_T_div_vol_mult_sqrt_t2, log_div_sok, vl);
 
 
-		vfloat32m4_t Time = vle_v_f32m4(T, vl);
-		vfloat32m4_t Time_mult_minus_int_rest = vfmult_vv_f32m4(Time, Minus_Int_rest, vl);
-		vse_v_f32m4(expmas, Time_mult_minus_int_rest, vl);
+		vfloat32m1_t Time = vle_v_f32m1(pT, vl);
+		vfloat32m1_t Time_mult_minus_int_rest = vfmul_vv_f32m1(Time, Minus_Int_rest, vl);
+		vse_v_f32m1(expmas, Time_mult_minus_int_rest, vl);
 		for (int i = 0; i < 4; i++)
 		{
 			expmas[i] = std::exp(expmas[i]);
 		}
-		vfloat32m4_t exp_Time_mult_minus_int_rest = vle_v_f32m4(expmas, vl);
-		vfloat32m4_t exp_etc_mult_d2 = vfmult_vv_f32m4(exp_Time_mult_minus_int_rest, d2, vl);
-		vfloat32m4_t so_mult_d1 = vfmult_vv_f32m4(so, d1, vl);
-		vfloat32m4_t so_mult_d1_minus_exp_etc_mult_d2 = vfsub_vv_f32m4(so_mult_d1, exp_etc_mult_d2, vl);
-		void vse_v_f32m4(C, so_mult_d1_minus_exp_etc_mult_d2, vl);
-		S0 += vl;
-		K += vl;
-		T += vl;
-		C += vl;
+		vfloat32m1_t exp_Time_mult_minus_int_rest = vle_v_f32m1(expmas, vl);
+		vfloat32m1_t exp_etc_mult_d2 = vfmul_vv_f32m1(exp_Time_mult_minus_int_rest, d2, vl);
+		vfloat32m1_t so_mult_d1 = vfmul_vv_f32m1(so, d1, vl);
+		vfloat32m1_t so_mult_d1_minus_exp_etc_mult_d2 = vfsub_vv_f32m1(so_mult_d1, exp_etc_mult_d2, vl);
+		vse_v_f32m1(pC, so_mult_d1_minus_exp_etc_mult_d2, vl);
+
+		//vse_v_f32m1(pC, so, vl);
+
+		pS0 += vl;
+		pK += vl;
+		pT += vl;
+		pC += vl;
 
 
 
 	}
+	
 }
-
-
-//void Option::Get_option_price()
-//{
-//#pragma omp parallel 
-//	{
-//#pragma omp for simd 
-//		for (int i = 0; i < N; i++)
-//		{
-//			float d1 = (std::log(S0[i] / K[i]) + ((Interest_rest + (Volatility * Volatility / 2.0f)) * T[i]) / Volatility * std::sqrtf(T[i]));
-//			float d2 = (std::log(S0[i] / K[i]) + ((Interest_rest - (Volatility * Volatility / 2.0f)) * T[i]) / Volatility * std::sqrtf(T[i]));
-//			float cdfnorm1 = 0.5f + std::erf(d1 / std::sqrtf(2.0f)) / 2.0f;
-//			float cdfnorm2 = 0.5f + std::erf(d2 / std::sqrtf(2.0f)) / 2.0f;
-//			C[i] = S0[i] * cdfnorm1 - K[i] * std::exp((-1.0f) * Interest_rest * T[i]) * cdfnorm2;
-//			//if (i == 0)
-//				//std::cout << C[i] << ' ' << d1 << ' ' << d2 << ' ' << cdfnorm1 << ' ' << cdfnorm2 << ' ' << T[i] << ' ' << S0[i] << ' ' << K[i] << std::endl;
-//		}
-//	}
-//}
